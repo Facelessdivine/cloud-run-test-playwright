086c79ed0e77ac9d7043107f84eb4ebfaa7a99d4
rmherrer <raul.herrera@autozone.com>
2026-01-31 22:07:30 -0600

    CR-Reviewed : fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,7 +10,7 @@ test("has title 2", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/fdsafsadfsadfad/);
 });
 test("has title 3", async ({ page }) => {
   await page.goto("https://playwright.dev/");
